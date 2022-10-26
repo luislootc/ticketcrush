fa9d367f25630138e7283cfffe1c712f66249663
luisloo10 <luis@ticketcrush.com.mx>
2022-10-26 09:14:22 -0500

    Adding new Ticket features

--- ticket/models.py
+++ ticket/models.py
@@ -1,18 +1,18 @@
 from cmd import IDENTCHARS
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
 
 class Ticket(models.Model):
+    eventID = models.IntegerField(null=True)
     Number = models.IntegerField()
-    Value = models.FloatField()
+    Price = models.FloatField(null=True)
+    QR = models.CharField(max_length=200, null=True)
+    Picture = models.CharField(max_length=200, null=True)
     
-
     def __str__(self):
         return str(self.Number)
 
     class Meta:
-        verbose_name_plural = "Ticket"
-
-
-    
+        verbose_name_plural = "Ticket"    
